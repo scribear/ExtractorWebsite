bbbdaeaa087910bf249d1195e3cfbbf8ad54d381
Tugoph <yunw3@illinois.edu>
2022-11-22 11:25:29 -0600

    Change to js

---
+++ src/pdfParser.js
@@ -0,0 +1,73 @@
+import pdfjsLib from 'pdfjs-dist';
+// import { GetDocumentParameters } from 'pdfjs-dist/types/src/display/api';
+
+document.getElementById('btn').addEventListener('click', convert);
+
+export const convert = () => {
+    let fr = new FileReader();
+    let pdff = Pdf2TextClass();
+    fr.onload = () => {
+        pdff.pdfToText(fr.result, null, (text) => { document.getElementById('result').innerText += text; });
+    }
+    fr.readAsDataURL(document.getElementById('pdffile').files[0])
+    
+}
+
+export const Pdf2TextClass = () => {
+    let self = this;
+    this.complete = 0;
+
+    this.pdfToText = (data, callbackPageDone, callbackAllDone) => {
+        console.assert(data instanceof ArrayBuffer || typeof data == 'string');
+        var loadingTask = pdfjsLib.getDocument(data);
+        loadingTask.promise.then(function (pdf) {
+
+
+            var total = pdf._pdfInfo.numPages;
+            //callbackPageDone( 0, total );        
+            var layers = {};
+            for (let i = 1; i <= total; i++) {
+                pdf.getPage(i).then(function (page) {
+                    var n = page.pageNumber;
+                    page.getTextContent().then(function (textContent) {
+
+                        //console.log(textContent.items[0]);0
+                        if (null != textContent.items) {
+                            var page_text = "";
+                            var last_block = null;
+                            for (var k = 0; k < textContent.items.length; k++) {
+                                var block = textContent.items[k];
+                                if (last_block != null && last_block.str[last_block.str.length - 1] != ' ') {
+                                    if (block.x < last_block.x)
+                                        page_text += "\r\n";
+                                    else if (last_block.y != block.y && (last_block.str.match(/^(\s?[a-zA-Z])$|^(.+\s[a-zA-Z])$/) == null))
+                                        page_text += ' ';
+                                }
+                                page_text += block.str;
+                                last_block = block;
+                            }
+
+                            textContent != null && console.log("page " + n + " finished."); //" content: \n" + page_text);
+                            layers[n] = page_text + "\n\n";
+                        }
+                        ++self.complete;
+                        //callbackPageDone( self.complete, total );
+                        if (self.complete == total) {
+                            window.setTimeout(function () {
+                                var full_text = "";
+                                var num_pages = Object.keys(layers).length;
+                                for (var j = 1; j <= num_pages; j++)
+                                    full_text += layers[j];
+                                callbackAllDone(full_text);
+                            }, 1000);
+                        }
+                    }); // end  of page.getTextContent().then
+                }); // end of page.then
+            } // of for
+        });
+    }; // end of pdfToText()
+}; // end of class
+
+export const test = () => {
+
+}
