817cec883c224a3a06538eb4b956317c3fbe1375
Heaper327 <jameszhu327@outlook.com>
2023-03-30 03:34:41 -0500

    Implementing word extraction in page parsing function

--- src/parser.js
+++ src/parser.js
@@ -1,5 +1,7 @@
+// import { text } from "stream/consumers";
 
 const setWords = new Set();
+var debug1, debug2, debug3;
 
 function isNumber(char) {
     return /^\d$/.test(char);
@@ -7,92 +9,132 @@ function isNumber(char) {
 
 function convert() {
     let fr = new FileReader();
-    let entire_text = "";
     console.debug("Start converting...")
 
     fr.readAsDataURL(document.getElementById('pdfFile').files[0])
     fr.onload = function(){ // this function is executed asynchronously when the pdf file has been loaded 
         //output is stored in result
-        pdfToText(fr.result, () => {}, (text) => { 
-            console.log(text)
-            entire_text += text;
-            document.getElementById('result').innerText += text;
+        pdfToWords(fr.result, () => {}, (words) => { 
+            words.map(word => setWords.add(word))
+            display_text = words.join('\n')
+            console.log(display_text)
+            document.getElementById('result').innerText = display_text;
             
-            let tempString = "";
-            for (const token of entire_text) {
-                if (!isNumber(token)) {
-                    tempString += token
-                } else {
-                    //console.log(tempString)
-                    if (tempString.length > 2) {
-                        setWords.add(tempString.trim())
-                        tempString = "";
-                    }
-                }
-            }
+            // let tempString = "";
+            // for (const token of entire_text) {
+            //     if (!isNumber(token)) {
+            //         tempString += token
+            //     } else {
+            //         //console.log(tempString)
+            //         if (tempString.length > 2) {
+            //             setWords.add(tempString.trim())
+            //             tempString = "";
+            //         }
+            //     }
+            // }
             
             const outString = JSON.stringify(Array.from(setWords));
-            // uploadFile(
-            //     outString,
-            //     "DomainWordExtractor",
-            //     "JoniLi99",
-            //     "main",
-            //     "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"
-            // )
+            uploadFile(
+                outString,
+                "DomainWordExtractor",
+                "JoniLi99",
+                "main",
+                "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"
+            )
             console.log(outString)
             createTxtFile(outString) 
         });
     }
-
-    console.log(document.getElementById('result').innerText)
-    
 }
 
-function pdfToText(data, callbackPageDone, callbackAllDone) {
+/**
+ * Structure of textContent:
+ * {
+ *      items: [
+ *          {
+ *              str: string, // content of the text block
+ *              width: float, // width of the text block
+ *              height: float, // height of the text block
+ *              dir: string, // "ltr" indicates horizontal text block
+ *              font_name: string, // key in styles dictionary
+ *              hasEOL: bool, 
+ *              transform: float[6] // meaning unknown
+ *          },
+ *          ...
+ *      ]
+ *      styles: {
+ *          font_name_1: {fontFamily: string, ascent: float, descent: float, vertical: bool},
+ *          font_name_2: {fontFamily: string, ascent: float, descent: float, vertical: bool},
+ *          ...
+ *      }
+ * }
+ */
+function textContentToWords(textContent) {
+    if (!textContent.items) {
+        return []
+    }
+    let words = []
+    // We assume here that an entry in the index has the format 'word word, page number' 
+    const entry_regex = new RegExp(/.*[a-zA-Z]+.*,.*/) // comma with 1+ alphabetical character somewhere before it
+    const word_regex = new RegExp(/.*?(?=,)/) // substring before the 1st comma
+    const brackets_regex = new RegExp(/\(.*?\)/) // a pair of brackets and whatever is in between them
+    for (const text_block of textContent.items) {
+        const text = text_block.str
+        if (!entry_regex.test(text)) {
+            continue
+        }
+        let word = text.match(word_regex)[0]
+        // TODO: remove extra spaces around brackets
+        word.replace(brackets_regex, "") // remove brackets
+        word = word.trim()
+        words.push(word)
+    }
+    debug1 = words
+    debug2 = textContent.items.map(item => item.str)
+    return words
+    // let page_text = "";
+    // let last_block = null;
+    // for (const text_block of textContent.items) {
+    //     if (last_block != null && last_block.str[last_block.str.length - 1] != ' ') {
+    //         if (text_block.x < last_block.x)
+    //             page_text += "\r\n";
+    //         else if (last_block.y != text_block.y && (last_block.str.match(/^(\s?[a-zA-Z])$|^(.+\s[a-zA-Z])$/) == null))
+    //             page_text += ' ';
+    //     }
+    //     page_text += text_block.str;
+    //     last_block = text_block;
+    // }
+
+    // return page_text + "\n";
+}
+
+function pdfToWords(data, callbackPageDone, callbackAllDone) {
     console.assert(data instanceof ArrayBuffer || typeof data == 'string');
 
     let loadingTask = pdfjsLib.getDocument(data);
     loadingTask.promise.then(function (pdf) {
 
         const num_pages = pdf._pdfInfo.numPages;
-        let layers = [];
+        let words = []
         let num_completed_pages = 0;
 
         for (let page_index = 1; page_index <= num_pages; page_index++) {
             pdf.getPage(page_index).then(function (page) {
-                const n = page.pageNumber; // Shouldn't i and n be equal?
-                if (page_index != n) console.log("Page number i and n not equal!")
-                
                 page.getTextContent().then(function (textContent) {
-                    //console.log(textContent.items[0]);0
-                    if (textContent.items) {
-                        let page_text = "";
-                        let last_block = null;
-                        for (const text_block of textContent.items) {
-                            if (last_block != null && last_block.str[last_block.str.length - 1] != ' ') {
-                                if (text_block.x < last_block.x)
-                                    page_text += "\r\n";
-                                else if (last_block.y != text_block.y && (last_block.str.match(/^(\s?[a-zA-Z])$|^(.+\s[a-zA-Z])$/) == null))
-                                    page_text += ' ';
-                            }
-                            page_text += text_block.str;
-                            last_block = text_block;
-                        }
 
-                        console.log("page " + page_index + " finished."); //" content: \n" + page_text);
-                        layers[page_index] = page_text + "\n";
-                    }
+                    words = words.concat(textContentToWords(textContent))
+
                     // Only call callback once all pages have been completely parsed
                     ++num_completed_pages;
+                    console.log("page " + page_index + " finished.");
+                    // TODO: replace this with something like a barrier sync primitive
                     if (num_completed_pages == num_pages && callbackAllDone) {
                         setTimeout(function () {
-                            let full_text = "";
-                            for (const layer of layers)
-                                full_text += layer;
-                            callbackAllDone(full_text);
+                            callbackAllDone(words);
                         }, 1000);
                     }
-                }); // end  of page.getTextContent().then
+
+                }); // end of page.getTextContent().then
             }); // end of page.then
         } // of for
     });
